efa9dedfec280aa5924d4dc02d0474cb99351506
Glen <glendorning@gmail.com>
2020-02-27 23:06:21 +0200

    changed variables

---
+++ tritable.py
@@ -0,0 +1,12 @@
+for a in range(1, 10):
+    for b in range(1,a+1):
+        
+        print(a*b, end=" ")
+    print("\n")
+       # print(str(a), end  =' ')
+       # print(str(b), end  =' ')
+        
+
+
+
+        
